0539847818ec06d43167e41fa2eb77ea4819899e
Faris Fadhail <farisfadhail@gmail.com>
2023-11-09 01:02:40 +0700

    fix: entities

--- models/entity/parentCategory.go
+++ models/entity/parentCategory.go
@@ -5,7 +5,7 @@ import "time"
 type ParentCategory struct {
 	ID                int               `gorm:"primaryKey"`
 	Name              string            `gorm:"size:255;column:name;uniqueIndex"`
-	ProductCategories []ProductCategory `gorm:"foreignKey:parent_category_id;references:id"`
+	ProductCategories []ProductCategory `json:"-" gorm:"foreignKey:parent_category_id;references:id"`
 	CreatedAt         time.Time         `gorm:"column:created_at;autoCreateTime"`
 	UpdatedAt         time.Time         `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
 }
--- models/entity/productCategory.go
+++ models/entity/productCategory.go
@@ -6,7 +6,7 @@ type ProductCategory struct {
 	ID               int       `gorm:"primaryKey"`
 	ParentCategoryId int       `gorm:"column:parent_category_id"`
 	Name             string    `gorm:"size:255;column:name;uniqueIndex"`
-	Products         []Product `gorm:"foreignKey:product_category_id;references:id"`
+	Products         []Product `json:"-" gorm:"foreignKey:product_category_id;references:id"`
 	CreatedAt        time.Time `gorm:"column:created_at;autoCreateTime"`
 	UpdatedAt        time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
 }
