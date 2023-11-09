1ba8c3d8f4df9ffa33456083f174e1cee410cbc6
Faris Fadhail <farisfadhail@gmail.com>
2023-11-09 23:05:25 +0700

    add: prepare product handler

---
+++ handler/ProductHandler.go
@@ -0,0 +1,23 @@
+package handler
+
+import "github.com/gofiber/fiber/v2"
+
+func GetAllProductHandler(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func StoreProductHandler(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func GetBySlugProductHandler(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func UpdateProductHandler(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func DeleteProductHandler(ctx *fiber.Ctx) error {
+	return nil
+}
