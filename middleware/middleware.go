35c65acf3c9daf5042dc2b6a90641151cd41a1cb
Faris Fadhail <farisfadhail@gmail.com>
2023-11-11 20:08:47 +0700

    feat: auth middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,26 @@
 package middleware
+
+import (
+	"plastindo-back-end/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Authenticated(ctx *fiber.Ctx) error {
+	token := ctx.Get("jwt-token")
+	if token == "" {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message" : "UNAUTHENTICATED",
+		})
+	}
+
+	_, err := utils.VerifyTokenJWT(token)
+	if err != nil {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message" : "UNAUTHENTICATED",
+			"error" : err.Error(),
+		})
+	}
+
+	return ctx.Next()
+}
